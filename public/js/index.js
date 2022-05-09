50f0b964ec676129868e9eecf54e5bbe3fd408ab
Sonarie <sonarie1980@gmail.com>
2022-05-08 20:45:38 -0500

    seperated the idb code to it's own js file

--- public/js/index.js
+++ public/js/index.js
@@ -78,64 +78,6 @@ function populateChart() {
   });
 }
 
-let db;
-const request = indexedDB.open('budget_tracker', 1);
-
-request.onupgradeneeded = function(event){
-    const db = event.target.result;
-    db.createObjectStore('new_transaction', {autoIncrement: true});
-}
-
-request.onsuccess = function(event){
-    db = event.target.result;
-    if (navigator.onLine) {
-        uploadTransaction();
-    }
-};
-
-request.onerror = function(event){
-    console.log(event.target.errorCode);
-};
-
-function saveRecord(record) {
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-    budgetObjectStore.add(record);
-};
-
-function uploadTransaction() {
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
-  const budgetObjectStore = transaction.objectStore('new_transaction');
-  const getAll = budgetObjectStore.getAll();
-
-  getAll.onsuccess = function () {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((serverResponse) => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-          const transaction = db.transaction(["new_transaction"], "readwrite");
-          const budgetObjectStore = transaction.objectStore("new_transaction");
-          budgetObjectStore.clear();
-
-          alert("All saved transactions has been submitted!");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  };
-}
-
 function sendTransaction(isAdding) {
   let nameEl = document.querySelector("#t-name");
   let amountEl = document.querySelector("#t-amount");
@@ -209,5 +151,3 @@ document.querySelector("#add-btn").onclick = function() {
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
-
-window.addEventListener("online", uploadTransaction);
